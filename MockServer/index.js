df40576715864bd2db0a278335d1f24e4faeeb94
SantiagoLazzari <slazzari@fi.uba.ar>
2017-09-14 21:54:20 -0300

    Added mock server

---
+++ MockServer/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// get request to update images
+app.get("/image_id/:imageId/density/:density", function (req, res) {
+  var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.density + "/" + req.params.imageId + ".png";
+
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+// get request to update images
+app.get("/update_images", function (req, res) {
+  var response = {response:[3, 4, 5]}
+
+  console.log("Sent updatable images " + response);
+  res.send(response)
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
