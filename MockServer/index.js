b0765a9a958e5db4398754a92b74b49874ab4ae6
SantiagoLazzari <slazzari@fi.uba.ar>
2017-09-16 16:51:28 -0300

    Change in mock server

--- MockServer/index.js
+++ MockServer/index.js
@@ -1,35 +1,45 @@
+// Setup
+
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
+
+// @GET
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// get request to update images
-app.get("/image_id/:imageId/density/:density", function (req, res) {
-  var options = {
-    root: __dirname + '/public/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  };
+app.get('/wiki', function (req, res) {
+  wiki = {data_one : 1,
+          data_two : "dataTwo"}
 
-  var fileName = req.params.density + "/" + req.params.imageId + ".png";
-
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', fileName);
-    }
-  });
+  res.send(wiki)
 });
+
+// app.get("/image_id/:imageId/density/:density", function (req, res) {
+//   var options = {
+//     root: __dirname + '/public/',
+//     dotfiles: 'deny',
+//     headers: {
+//         'x-timestamp': Date.now(),
+//         'x-sent': true
+//     }
+//   };
+//
+//   var fileName = req.params.density + "/" + req.params.imageId + ".png";
+//
+//   res.sendFile(fileName, options, function (err) {
+//     if (err) {
+//       console.log(err);
+//       res.status(err.status).end();
+//     }
+//     else {
+//       console.log('Sent:', fileName);
+//     }
+//   });
+// });
 
 // get request to update images
 app.get("/update_images", function (req, res) {
